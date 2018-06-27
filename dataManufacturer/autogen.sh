3d4739700212a0a88d7ffe0608bed8723d1efb47
CompassHXM <huangxm9051@gmail.com>
2018-06-27 11:13:11 +0800

    add CDA

--- dataManufacturer/autogen.sh
+++ dataManufacturer/autogen.sh
@@ -2,19 +2,24 @@
 set -e #x to display commands, e to exit when error
 inputfile="$1"
 pattern=${inputfile%.txt}
-limit=60000
+Louvain_src="./Louvain_ver_0.3"
+limit=4
 
 if [ ! -f ${inputfile} ]; then
 	echo file ${inputfile} is not exist.
 	exit 1
 fi
 
+echo building...
+cd ${Louvain_src} && make && cd -
+g++ placement.cpp -std=c++11 -O2 -o placement
+
 echo detected pattern as "$pattern"
 set -x
 if [ ! -f ${pattern}.bin ]; then
-	./convert -i ${inputfile} -o ${pattern}.bin
+	./${Louvain_src}/convert -i ${inputfile} -o ${pattern}.bin
 fi
-./louvain ${pattern}.bin -l -1 > ${pattern}.tree
+./${Louvain_src}/louvain ${pattern}.bin -l -1 > ${pattern}.tree
 ./placement ${pattern}.tree -l ${limit} -m ${limit} -p ${pattern}.plan.json > ${pattern}.renum
 ./graph_renumber.py $1 -r ${pattern}.renum -o ${pattern}.txt.new
 
